1d6c585095bafd211e133b4fe5bf0ee34ea3c669
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-08-20 20:25:02 -0300

    test: config rspec

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if ['development', 'test'].include? ENV['RAILS_ENV'] 
+  Dotenv::Railtie.load
+end
+
 module EcommerceBootcamp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
